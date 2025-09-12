5a51fbaa1ec4949519ef2e0c0f6e8a92aae47ede
Aneesh Sharma <aneeshsharma@resiliencesoft.com>
2025-09-12 12:42:08 +0530

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { registerUser } from './users/users.controller';
+import { closeReport, createReport } from './reports/reports.controller';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  return res.json({ message: 'Yoooooooooooooo' });
+});
+
+app.post('/auth/users/register', (req, res) => {
+  return res.json({ message: 'ROute working' });
+});
+
+app.post('/auth/users/register', registerUser);
+app.post('/reports/create', createReport);
+app.post('/reports/close', closeReport);
+
+export default app;
---
+++ src/reports/reports.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import { sendEmail } from '../emails/emailService';
+
+export const createReport = async (req: Request, res: Response) => {
+  try {
+    const { email, message } = req.body;
+
+    await sendEmail(email, message, 'report-created.html', {
+      username: email,
+    });
+
+    return res.json({ message: 'Report created & email sent!' });
+  } catch (err) {
+    console.log('process.env.SMTP_HOST', process.env.SMTP_HOST);
+    console.log('process.env.SMTP_USER', process.env.SMTP_USER);
+    console.log('process.env.SMTP_PASS', process.env.SMTP_PASS);
+
+    console.log(err);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export const closeReport = async (req: Request, res: Response) => {
+  try {
+    const { email, message } = req.body;
+
+    await sendEmail(email, message, 'report-closed.html', {
+      username: email,
+    });
+
+    return res.json({ message: 'Report closed & email sent!' });
+  } catch (err) {
+    console.log('process.env.SMTP_HOST', process.env.SMTP_HOST);
+    console.log('process.env.SMTP_USER', process.env.SMTP_USER);
+    console.log('process.env.SMTP_PASS', process.env.SMTP_PASS);
+
+    console.log(err);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
